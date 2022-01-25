48d34434fd5132dc08a55439163ff46019825fb7
tankxu <hellotank@qq.com>
2022-01-25 19:37:13 +0800

    Version 1.0.0

---
+++ code.ts
@@ -0,0 +1,51 @@
+function main () {
+  if (figma.currentPage.selection.length !== 2) {
+    figma.closePlugin("⚠️ Please select 2 layers.");
+    return
+  } 
+  figma.showUI(__html__);
+
+  let selection1 = figma.currentPage.selection[0]
+  let selection2 = figma.currentPage.selection[1]
+
+  figma.ui.postMessage([selection1.name, selection2.name])
+
+  figma.ui.onmessage = msg => {
+
+    if (msg.type === 'selection-order') {
+      console.log(msg.order)
+      let originLayer, targetLayer
+      if (msg.order) {
+        originLayer = selection2
+        targetLayer = selection1
+      } else {
+        originLayer = selection1
+        targetLayer = selection2
+      }
+
+      let paint = originLayer.fills[0];
+      // console.log(paint);
+
+      originLayer
+        .exportAsync({ constraint: { type: "SCALE", value: 2 }, format: "PNG" })
+        .then((bytes) => {
+          // console.log(bytes);
+          const image = figma.createImage(bytes);
+
+          let newPaint = [];
+          newPaint = JSON.parse(JSON.stringify(targetLayer.fills));
+          newPaint.push({
+            imageHash: image.hash,
+            scaleMode: "TILE",
+            scalingFactor: 0.5,
+            type: "IMAGE",
+          });
+          // console.log(newPaint);
+          targetLayer.fills = newPaint;
+        });
+    }
+    figma.closePlugin();
+  };
+}
+
+main()
